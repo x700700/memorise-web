a73df7d330a8f2010d8953276c89748044591223
x700700@gmail.com <booli@feature.fm>
2019-11-30 14:09:05 +0200

    add fontawsome icons

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Game.scss';
 import Card from "../components/Practice/Card";
-import Rotate90DegreesCcwTwoToneIcon from '@material-ui/icons/Rotate90DegreesCcwTwoTone';
+// import Rotate90DegreesCcwTwoToneIcon from '@material-ui/icons/Rotate90DegreesCcwTwoTone';
 
 function Game() {
     return (
         <div className="game">
             <Card q="Many"/>
             <div className="rotate-icon">
-                <Rotate90DegreesCcwTwoToneIcon fontSize="large"/>
+                <button className="btn"><i className="fas fa-sync-alt"></i></button>
             </div>
         </div>
     );
