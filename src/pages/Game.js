94adb382ad2bc70d0101c976bcc2f7a15a29cab3
x700700@gmail.com <booli@feature.fm>
2019-11-30 13:41:49 +0200

    material ui + icons

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Game.scss';
 import Card from "../components/Practice/Card";
+import Rotate90DegreesCcwTwoToneIcon from '@material-ui/icons/Rotate90DegreesCcwTwoTone';
 
 function Game() {
     return (
         <div className="game">
             <Card q="Many"/>
-            <h1>Rotate</h1>
+            <div className="rotate-icon">
+                <Rotate90DegreesCcwTwoToneIcon fontSize="large"/>
+            </div>
         </div>
     );
 }
