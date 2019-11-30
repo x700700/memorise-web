10dab7733a2376a0ab75371a4ee1a295317d0dc7
x700700@gmail.com <booli@feature.fm>
2019-11-30 12:35:08 +0200

    node 10 + code refactor

---
+++ src/pages/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Game.scss';
+
+function Game() {
+    return (
+        <div className="Game">
+            <h1>hellow</h1>
+            <h2>world</h2>
+        </div>
+    );
+}
+
+export default Game;
