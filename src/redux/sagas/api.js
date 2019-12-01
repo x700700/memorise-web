efa2be8ecdfa6608eaeb00b3e1faa1991c602eb1
x700700@gmail.com <booli@feature.fm>
2019-12-01 19:00:35 +0200

    saga refactor...

--- src/redux/sagas/api.js
+++ src/redux/sagas/api.js
@@ -1,15 +1,6 @@
-import Axios from 'axios';
+import axios from '../../middlewares/axios';
 import consts from '../../common/consts';
 
-const axios = Axios.create({
-    timeout: 10 * 1000,
-    headers: {
-        'Authorization': `Bearer ${consts.temp.bearer}`,
-        'Content-Type': 'application/json',
-    },
-});
-
 export const auth = ({ bearer }) => {
-    return axios.get(`${consts.urls.api}/auth/check`)
-        .then(response => response.data);
+    return axios({method: 'GET', url: `${consts.urls.api}/auth/check`});
 };
