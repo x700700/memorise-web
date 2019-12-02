6696c68505eca5fb811805ef9ac244f68a64af47
x700700@gmail.com <booli@feature.fm>
2019-12-02 11:15:19 +0200

    game dom redesign++ for desktop

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -51,24 +51,26 @@ const Game = (props) => {
     const currQ = (top || {}).q || '';
     const currA = (top || {}).a || '';
     return (
-        <div className="game-container">
-            {(currQ || gameEnded) &&
-            <div className="game">
-                {currQ &&
-                <div className="cards-left">
-                    {curr} / {size}
-                </div>
-                }
-                <Card ref={refGame} q={currQ} a={currA} setCardInMove={setCardInMove}/>
-                <div className={`game-buttons ${cardInMove || gameEnded ? 'buttons-disable' : ''}`}>
-                    <button onClick={respBad} className="btn btn-bad"><i className="fas fa-times"></i></button>
-                    <button onClick={rotateCard} className="btn"><i className="fas fa-sync-alt"></i></button>
-                    <button onClick={respGood} className="btn btn-good"><i className="fas fa-check"></i></button>
-                </div>
-            </div>}
-            <PopUpBox show={gameEnded}>
-                <GameSum setStats={gameEnded} cardsNum={size} playsNum={playsNum} replayGame={() => replayGame}/>
-            </PopUpBox>
+        <div className="game-desktop-container">
+            <div className="game-container">
+                {(currQ || gameEnded) &&
+                <div className="game">
+                    {currQ &&
+                    <div className="cards-left">
+                        {curr} / {size}
+                    </div>
+                    }
+                    <Card ref={refGame} q={currQ} a={currA} setCardInMove={setCardInMove}/>
+                    <div className={`game-buttons ${cardInMove || gameEnded ? 'buttons-disable' : ''}`}>
+                        <button onClick={respBad} className="btn btn-bad"><i className="fas fa-times"></i></button>
+                        <button onClick={rotateCard} className="btn"><i className="fas fa-sync-alt"></i></button>
+                        <button onClick={respGood} className="btn btn-good"><i className="fas fa-check"></i></button>
+                    </div>
+                </div>}
+                <PopUpBox show={gameEnded}>
+                    <GameSum setStats={gameEnded} cardsNum={size} playsNum={playsNum} replayGame={() => replayGame}/>
+                </PopUpBox>
+            </div>
         </div>
     );
 };
