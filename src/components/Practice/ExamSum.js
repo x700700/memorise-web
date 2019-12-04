ec7cb5112106b9db22d9ad1c307cbf6c6f9be802
x700700@gmail.com <booli@feature.fm>
2019-12-04 22:10:30 +0200

    react-vis

--- src/components/Practice/ExamSum.js
+++ src/components/Practice/ExamSum.js
@@ -2,12 +2,12 @@ import React, {forwardRef, useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 import Button from "../common/Button";
 import './ExamSum.scss';
+import { RadialChart } from 'react-vis';
 
 const ExamSum = forwardRef(({ setStats, cardsNum, rightsNum, replayExam }, ref) => {
     const { t } = useTranslation();
     const [cards, setCards] = useState(0);
     const [rights, setRights] = useState(0);
-    const score = Math.round(rightsNum / cardsNum * 100);
 
     useEffect(() => {
         if (setStats) {
@@ -16,6 +16,7 @@ const ExamSum = forwardRef(({ setStats, cardsNum, rightsNum, replayExam }, ref) 
         }
     }, [setStats, cardsNum, rightsNum]);
 
+    const score = Math.round(rights / cards * 100);
     return (
         <div className="exam-sum-container">
             <div className="sum-col">
@@ -23,6 +24,16 @@ const ExamSum = forwardRef(({ setStats, cardsNum, rightsNum, replayExam }, ref) 
                     <p>{cards} cards played</p>
                     <br/>
                     <div className="exam-score">Score: {score}</div>
+                    {false &&
+                    <div className="exam-score-pie">
+                        <RadialChart
+                            width={170}
+                            height={170}
+                            data={[{angle: 3, radius: 50}, {angle: 8, radius: 14}]}
+                        >
+                        </RadialChart>
+                    </div>
+                    }
                 </div>
                 <div className="btns-container">
                     <div className="btns-replay">
