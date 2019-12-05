adaa18b6cd7e41d8826e7744ae059484ad1a6ef9
x700700@gmail.com <booli@feature.fm>
2019-12-05 16:47:25 +0200

    styling...

--- src/components/TrainingsList/SampleExercise.js
+++ src/components/TrainingsList/SampleExercise.js
@@ -2,15 +2,19 @@ import React from "react";
 import './SampleExercise.scss';
 
 const Divider = ({ width }) => {
-
+    return (
+        <div className="divider-container">
+            <div className="divider" style={{ width: width }}></div>
+        </div>);
 };
 
 const SampleExercise = ({ q, a }) => {
     return (
         <div className="sample-exercise-container">
             <div className="sample-exercise-col">
-                <div className="sample-q">{q}</div>
-                <div className="sample-a">{a}</div>
+                <div className="sample sample-q">{q}</div>
+                {/*<Divider width="80%"/>*/}
+                <div className="sample sample-a">{a}</div>
             </div>
 
         </div>);
