deb3389231667c32117353f64cab84ce62009a9c
x700700@gmail.com <booli@feature.fm>
2019-12-05 13:01:06 +0200

    redux refactor

--- src/middlewares/store.js
+++ src/middlewares/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as formReducer } from 'redux-form';
 import { createLogger } from 'redux-logger'
-import reducers from '../redux/reducers';
+import reducers from '../redux/store';
 import middleware from './saga';
 
 const logger = createLogger({
