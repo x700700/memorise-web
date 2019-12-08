fb68aedbb2d2252435a245f1fe1ea92a722be4b4
x700700@gmail.com <booli@feature.fm>
2019-12-09 00:26:50 +0200

    DrawerButtons imp...

--- src/components/TrainingsList/DrawerButtons.js
+++ src/components/TrainingsList/DrawerButtons.js
@@ -9,6 +9,15 @@ const DrawerButtons = ({ size, color, backgroundColor, backgroundColorDraw }) =>
         setOpen(!open);
     };
 
+    const numOfButtons = 2;
+    const styleDrawer = {
+        width: `${(size + 1) * (numOfButtons + 1)}rem`,
+        marginLeft: `-${(size + 1) * numOfButtons}rem`,
+        boxShadow: open ? '5px 5px 14px -4px #aaa' : 'none',
+    };
+    const styleOpenBtn = {
+        height: `${size + 1}.1rem`
+    };
     const styleBg = {
         backgroundColor: `#${backgroundColor}`,
     };
@@ -21,14 +30,14 @@ const DrawerButtons = ({ size, color, backgroundColor, backgroundColorDraw }) =>
     };
 
     return (
-        <div className="drawer-buttons-container">
+        <div className="drawer-buttons-container" style={styleDrawer}>
             <div className="buttons-absolute" style={{ ...styleAbsolute, ...styleBgDraw}}>
                 <div className="btns-row">
                     <IconButton faName="running" size={size}/>
                     <IconButton faName="grin-beam-sweat" size={size}/>
                 </div>
             </div>
-            <div className="open-button">
+            <div className="open-button" style={styleOpenBtn}>
                 <IconButton faName="bars" size={size} onClick={clicked}/>
             </div>
             <div className="open-button-absolute" style={styleBg}>
