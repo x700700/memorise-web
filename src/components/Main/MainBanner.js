69661ed3e3853889248d55db1e85e36176b86fd7
x700700@gmail.com <booli@feature.fm>
2019-12-22 03:54:39 +0200

    MainBanner style

--- src/components/Main/MainBanner.js
+++ src/components/Main/MainBanner.js
@@ -3,6 +3,7 @@ import './MainBanner.scss';
 import {useDispatch, useSelector} from "react-redux";
 import consts from "../../common/consts";
 import * as types from "../../redux/actionsTypes";
+import logo from "../../logo.svg";
 
 
 const MainBanner = (props) => {
@@ -22,11 +23,18 @@ const MainBanner = (props) => {
         <div className="main-banner" style={styleBanner}>
             <div className="main-banner-header-space-holder" style={{ minHeight: `${consts.ui.headerHeight}px` }}></div>
             <div className="main-banner-header" style={{ minHeight: `${consts.ui.headerHeight + 2}px` }}>
-                <div className="banner-header-row" onClick={close}>
-                    Main banner Header..!
+                <div className="banner-header-row">
+                    <div className="header-buttons" onClick={close}>
+                        <img className="logo" src={logo} alt="logo" width="32" height="32" />
+                        <i className='fas fa-chevron-right icon-close'/>
+                    </div>
                 </div>
             </div>
-            Main Banner....
+            <div className="banner-body-container">
+                <div className="banner-body" style={{ height: `calc(100% - ${consts.ui.headerHeight + 10}px)` }}>
+                    Main Banner....
+                </div>
+            </div>
         </div>);
 };
 export default MainBanner;
