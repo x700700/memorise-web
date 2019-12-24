191b79cbc1222ccf5f85f92c3fb365d387291f52
x700700@gmail.com <booli@feature.fm>
2019-12-24 07:44:29 +0200

    Toggle styling

--- src/components/_Tools/Toggle.js
+++ src/components/_Tools/Toggle.js
@@ -1,5 +1,5 @@
 import React, {forwardRef, useEffect, useImperativeHandle, useState} from 'react';
-import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import {createMuiTheme, makeStyles, ThemeProvider} from "@material-ui/core/styles";
 import MaterialSwitch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import {purple} from "@material-ui/core/colors";
@@ -13,9 +13,37 @@ const Toggle = forwardRef(({ muiTheme, label, color, value, onChange }, ref) => 
         },
     }));
 
+    const _color = color || purple;
     const theme = createMuiTheme({
         palette: {
-            primary: color || purple,
+            primary: _color,
+        },
+    });
+    const useStyles = makeStyles({
+        switchBase: {
+            color: _color[200],
+            '&$checked': {
+                color: _color[500],
+            },
+            '&$checked + $track': {
+                backgroundColor: _color[500],
+            },
+            // padding: 4,
+            // transform: 'translateY(.7px)',
+        },
+        /*
+        root: {
+            width: 50,
+            height: 26,
+            padding: 0,
+            margin: theme.spacing(1.5),
+            marginLeft: 0,
+        },
+         */
+        checked: {},
+        track: {
+            border: `.5px solid ${_color[500]}`,
+            backgroundColor: '#ccc',
         },
     });
 
@@ -28,12 +56,21 @@ const Toggle = forwardRef(({ muiTheme, label, color, value, onChange }, ref) => 
         setCheck(value || false);
     }, [value, setCheck]);
 
+    const classes = useStyles();
     return (
         <div>
             <ThemeProvider theme={theme}>
                 <FormControlLabel
                     control={
-                        <MaterialSwitch color="primary" checked={check} onChange={handleChange}/>
+                        <MaterialSwitch color="primary"
+                                        classes={{
+                                            root: classes.root,
+                                            switchBase: classes.switchBase,
+                                            thumb: classes.thumb,
+                                            track: classes.track,
+                                            checked: classes.checked,
+                                        }}
+                                        checked={check} onChange={handleChange}/>
                     }
                     label={label}
                     // labelPlacement="start"
